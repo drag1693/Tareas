8c4eeb3268e3ad3b4dbee307cdba454010b51c0f
drag1693 <99093709+drag1693@users.noreply.github.com>
2022-02-05 18:43:08 -0600

    Tarea Actividad 2
    Mi primera tarea en  Github

---
+++ Actividad2.js
@@ -0,0 +1,46 @@
+var mes = 10
+var nombre= ("Dulce Rocío Alfaro Girón")
+var carnet= (22000343)
+
+
+switch (mes){
+case 1:
+alert("Es invierno");
+    break;
+case 2:
+alert("Es invierno");
+    break;
+case 3:
+    alert("Es primavera");
+    break;
+case 4:
+    alert("Es primavera");
+    break;
+case 5:
+    alert("Es primavera");
+    break;
+case 6:
+    alert("Es verano");
+    break;
+case 7:
+    alert("Es verano");
+    break;
+case 8:
+    alert("Es verano");
+    break;
+case 9:
+    alert("Es otoño");
+    break;
+case 10:
+    alert("Es otoño");
+    break;
+case 11:
+    alert("Es otoño");
+    break;
+case 12:
+alert("Es invierno");
+    break;
+}
+
+alert(nombre)
+alert(carnet)
